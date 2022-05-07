65e47dc1670ef19b7a0d30f4014d9d97b6681fa3
iryna-ag <iryna.agafonov@gmail.com>
2022-05-07 10:31:28 -0400

    picture

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.png";
+import book from "./book.avif";
 import "./App.css";
 import Dictionary from "./Dictionary";
 
@@ -7,36 +7,13 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <img src={book} className="App-book img-fluid" alt="book" />
         </header>
         <main>
-          <Dictionary defaultKeyword="sunset" />
+          <Dictionary />
         </main>
-        <footer>
-          This project was coded by{" "}
-          <a
-            href="https://portfolioirynaagafonov.netlify.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Iryna Agafonov
-          </a>{" "}
-          and is{" "}
-          <a
-            href="https://github.com/iryna-ag/weather-due"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            open-sourced on GitHub
-          </a>{" "}
-          and{" "}
-          <a
-            href="https://weatherreactappfinal.netlify.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            hosted on Netlify
-          </a>
+        <footer className="App-footer">
+          <small>Coded by Iryna Agafonov</small>
         </footer>
       </div>
     </div>
