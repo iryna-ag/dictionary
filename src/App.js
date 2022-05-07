3ca1b1f22578c1c10c567cafe992bd41415cb004
iryna-ag <iryna.agafonov@gmail.com>
2022-05-07 19:08:45 -0400

    footer

--- src/App.js
+++ src/App.js
@@ -9,9 +9,34 @@ export default function App() {
         <main>
           <Dictionary />
         </main>
-        <footer className="App-footer">
-          <small>Coded by Iryna</small>
-        </footer>
+        <div>
+          <footer>
+            This project was coded by{" "}
+            <a
+              href="https://portfolioirynaagafonov.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Iryna Agafonov
+            </a>{" "}
+            and is{" "}
+            <a
+              href="https://github.com/iryna-ag/weather-due"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              open-sourced on GitHub
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://weatherreactappfinal.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              hosted on Netlify
+            </a>
+          </footer>
+        </div>{" "}
       </div>
     </div>
   );
