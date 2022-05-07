514a90e3a182999116504d920640786e19f7cf70
iryna-ag <iryna.agafonov@gmail.com>
2022-05-07 12:50:12 -0400

    font awesome

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -5,8 +5,9 @@ export default function Phonetic(props) {
   return (
     <div className="Phonetic">
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-        Listen
+        Listen Pronunciation
       </a>
+      <i class="fa-solid fa-ear-listen"></i>
       <span className="text">{props.phonetic.text}</span>
     </div>
   );
