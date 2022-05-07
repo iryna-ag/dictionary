3922f2538253edf082197bb8e951cd85d1ac4480
iryna-ag <iryna.agafonov@gmail.com>
2022-05-07 11:33:56 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import book from "./book.avif";
+import logo from "./logo.png";
 import "./App.css";
 import Dictionary from "./Dictionary";
 
@@ -7,13 +7,13 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <img src={book} className="App-book img-fluid" alt="book" />
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
         </header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="sunset" />
         </main>
         <footer className="App-footer">
-          <small>Coded by Iryna Agafonov</small>
+          <small>Coded by Iryna</small>
         </footer>
       </div>
     </div>
