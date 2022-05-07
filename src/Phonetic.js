661d1460210e56b8d4d0341ef7fbda0059d844a0
iryna-ag <iryna.agafonov@gmail.com>
2022-05-07 13:47:15 -0400

    photos

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -7,7 +7,6 @@ export default function Phonetic(props) {
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         Listen Pronunciation
       </a>
-      <i class="fa-solid fa-ear-listen"></i>
       <span className="text">{props.phonetic.text}</span>
     </div>
   );
